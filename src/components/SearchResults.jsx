eb9770f7343b86bac319c4dd12648f9b679f4fc9
Chris Winfrey <christopher.winfrey@endurance.com>
2017-03-26 13:17:27 -0700

    Layout changes

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -17,29 +17,30 @@ class SearchResults extends Component {
 
     return (
     <div>
-      <h3 className="header flow-text">{"Search Results" + " for " + "'" + this.props.searchTerm + "'"}</h3>
-      <div className="row">
+      <h3 className="header">{"Displaying " + results.length + " results " + " for " + "'" + this.props.searchTerm + "'"}</h3>
           { results.map((result, i) => {
             return (
-          <div className="col m10">
-            <div key={i} className="card horizontal z-depth-2">
-              <div className="card-image">
-                <img alt="Poster" src={result.poster}></img>
+        <div className="row">
+          <div className="col m12">
+            <div>
+              <div className="col m4">
+                <img key={i} className="materialboxed" style={{height: "250px", width:"200px", marginTop: "25px"}} src={result.poster}></img>
               </div>
-              <div className="card-content">
-                <h3 className="flow-text"><b>{result.show_title}</b></h3>
-                <p className="flow-text">Director: {result.director}</p>
-                <p className="flow-text">Cast: {result.show_cast}</p>
-                <p className="flow-text">Summary: {result.summary}</p>
-              </div>
-              <div className="card-action">
+              <div style={{marginTop: "1%"}} className="col m8">
+                <ul className="collection with-header">
+                  <li className="collection-header"><h4>{result.show_title}</h4></li>
+                  <li className="collection-item">{"Director:" + " " + result.director}</li>
+                  <li className="collection-item">{"Starring:" + " " + result.show_cast}</li>
+                  <li className="collection-item">{"Summary:"+ " " + result.summary}</li>
+                  {/*<li className="collection-item">Alvin</li>*/}
+                </ul>
               </div>
             </div>
           </div>
-          )
-        })
-      }
-      </div>
+        </div>
+            )
+          })
+        }
     </div>
     )
   }
